3cd7eb6f279e07ca07017afc5835714db90377b1
moawezz <moawez.de@gmail.com>
2024-01-04 17:44:42 +0330

    change start_handler name to command_handler

---
+++ plugins/command_handler.py
@@ -0,0 +1,40 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from constants.join_checker_filter import is_user_joined
+from models.users import User
+from constants.bot_messages import WELCOME_MESSAGE, INLINE_SEARCH_BODY
+from constants.keyboards import INLINE_SEARCH_BUTTON
+
+@Client.on_message(filters.command('start'))
+async def start(client: Client, message: Message):
+    chat_id = message.chat.id
+    first_name = message.from_user.first_name
+    username = message.from_user.username
+    
+    if not await is_user_joined(None, client, message):
+        return
+
+    user = User.get_or_none(User.chat_id == chat_id)
+    if user:
+        user.first_name = first_name
+        user.username = username
+        user.save()
+    else:
+        User.create(
+            chat_id=chat_id,
+            first_name=first_name,
+            username=username,
+        )
+    await message.reply_text(WELCOME_MESSAGE)
+
+@Client.on_message(filters.command('search'))
+async def search(client: Client, message: Message):
+    chat_id = message.chat.id
+    if not await is_user_joined(None, client, message):
+        return
+    
+    await client.send_message(
+        chat_id=chat_id,
+        text=INLINE_SEARCH_BODY,
+        reply_markup=INLINE_SEARCH_BUTTON
+    )
