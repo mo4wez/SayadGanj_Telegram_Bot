a7c264232c026b5f0ac20fac91b1f04f60ce1176
moawezz <moawez.de@gmail.com>
2024-07-03 23:47:19 +0330

    change models based on new db

--- models/words.py
+++ models/words.py
@@ -1,19 +1,19 @@
-from peewee import Model, SqliteDatabase, CharField, PrimaryKeyField
+from peewee import Model, SqliteDatabase, CharField, TextField, IntegerField
 
-db = SqliteDatabase(r'C:\Users\moawe\Desktop\sayadganj_bot\db\wordbook.db')
+db = SqliteDatabase(r'C:\Users\moawe\Desktop\sayadganj_bot\db\sayadganj.db')
 
-class WordBook(Model):
-    _id = PrimaryKeyField()
-    betaNoSymbols = CharField()
-    betaSymbols = CharField()
-    langNoSymbols = CharField()
-    langLowercase = CharField()
-    langFullWord = CharField()
-    entry = CharField()
-    soundName = CharField()
 
+class BaseModel(Model):
     class Meta:
         database = db
-        table_name = 'wordbook'
+
+class SayadGanj(BaseModel):
+    id = IntegerField(primary_key=True)
+    full_word = CharField()
+    full_word_with_symbols = CharField()
+    definition = TextField()
+
+    class Meta:
+        table_name = 'sayadganj'
 
 db.connect()
