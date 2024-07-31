6d235c2156625d8357be2a1bbe4a94ee6e9418f2
moawezz <moawez.de@gmail.com>
2024-07-31 04:19:08 +0330

    feat: Add start_date field for new users in database

--- models/users.py
+++ models/users.py
@@ -1,4 +1,5 @@
-from peewee import Model, SqliteDatabase, CharField
+from peewee import Model, SqliteDatabase, CharField, DateField, OperationalError
+from datetime import datetime
 
 db = SqliteDatabase(r'C:\Users\moawe\Desktop\sayadganj_bot\db\users.db')
 
@@ -6,9 +7,23 @@ class User(Model):
     chat_id = CharField(unique=True)
     first_name = CharField()
     username = CharField(null=True)
+    start_date = DateField(default=datetime.now().date)
 
     class Meta:
         database = db
 
+
+def add_start_date_column():
+    # Raw SQL to add the new column if it doesn't exist
+    try:
+        db.execute_sql('ALTER TABLE user ADD COLUMN start_date DATE')
+    except OperationalError as e:
+        if "duplicate column name: start_date" in str(e):
+            # The column already exists, ignore the error
+            pass
+        else:
+            raise e
+        
 db.connect()
+add_start_date_column()
 db.create_tables([User], safe=True)
